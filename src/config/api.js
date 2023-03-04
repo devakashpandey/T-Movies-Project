e978a58f3945e67036d48a9c3a5290f5df8410e1
DevAkky <developerakky@gmaiil.com>
2023-03-04 15:16:12 +0530

    setup redux toolkit

---
+++ src/config/api.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const BASE_URL = "https://api.themoviedb.org/3";
+
+const TMBD_TOKEN =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjOTEyNWUwMDZkZTFkYzJjNjQxMDJjMWU1ZTk4NzZhZiIsInN1YiI6IjY0MDFiNDc4OWYxYmU3MDBjZDYyZTAxNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Eb4u5g_2_TP9kXWIh6jKwRQMjDBqxzEzeptZvTwuPEY";
+
+const headers = {
+  Authorization: "bearer " + TMBD_TOKEN,
+};
+
+export const fetchDataFromApi = async (url, params) => {
+  try {
+    const { data } = await axios.get(BASE_URL + url, {
+      headers,
+      params,
+    });
+    return data;
+  } catch (err) {
+    toast.error(err);
+    return err;
+  }
+};
