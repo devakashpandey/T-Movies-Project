e71c71dcdd20dc004f5106d12755d448c13e8ff1
Akash Pandey <114848776+devakashpandey@users.noreply.github.com>
2023-03-11 23:42:32 +0530

    Update ContentWrapper.js

--- src/components/contentWrapper/ContentWrapper.js
+++ src/components/contentWrapper/ContentWrapper.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Component.css";
 
+// a responsive container for gap from both left and right side
 const ContentWrapper = ({ children }) => {
   return <div className="content-wrapper">{children}</div>;
 };
