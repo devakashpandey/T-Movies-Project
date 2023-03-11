3f488ba7d290d045a39b5996b4746bebcc022095
Akash Pandey <114848776+devakashpandey@users.noreply.github.com>
2023-03-11 23:53:40 +0530

    Update HeroBanner.js

--- src/pages/home/heroBanner/HeroBanner.js
+++ src/pages/home/heroBanner/HeroBanner.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import useFetch from "../../../hooks/useFetch";
 import { useSelector } from "react-redux";
 import Img from "../../../components/lazyLoadImg/Img";
-import ContentWrapper from "../../../components/contentWrapper/ContentWrapper"; // fro centering the all content
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper"; // for centering the all content
 
 const HeroBanner = () => {
   const [bgBanner, setBgBanner] = useState("");
