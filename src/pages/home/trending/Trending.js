f7d5fec614b1b14a0fe42c55b2f3e5b279d23f1d
DevAkky <developerakky@gmail.com>
2023-03-15 14:48:40 +0530

    api call

--- src/pages/home/trending/Trending.js
+++ src/pages/home/trending/Trending.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Style.scss";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
+import useFetch from "../../../hooks/useFetch";
 
 const Trending = () => {
-  const onTabChange = (tab) => {};
+  const [endpoint, setEndpoint] = useState("day");
+
+  const { data, loading } = useFetch(`/trending/all/${endpoint}`);
+
+  const onTabChange = (tab) => {
+    setEndpoint(tab === "Day" ? "day" : "week");
+  };
 
   return (
     <>
       <div className="carousel-Section">
         <ContentWrapper>
           <span className="carousel-title">Trending</span>
-          <SwitchTabs
-            data={["Day", "Week", "Month"]}
-            onTabChange={onTabChange}
-          />
+          <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
         </ContentWrapper>
       </div>
     </>
