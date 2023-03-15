92518c2b07563472b2e1bca48b48dcd1f3a3cfd2
DevAkky <developerakky@gmail.com>
2023-03-15 14:35:10 +0530

    caraousel toggle design done

--- src/components/switchTabs/SwitchTabs.js
+++ src/components/switchTabs/SwitchTabs.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SwitchTabs.scss";
 
-const SwitchTabs = () => {
+const SwitchTabs = ({ data, onTabChange }) => {
+  const [select, setSelect] = useState(0); // for active select option
+
+  const activeTab = (tab, index) => {
+    setTimeout(() => {
+      setSelect(index);
+    }, 300); // for smooth animation
+    onTabChange(tab, index);
+  };
+
   return (
     <>
-      <h1>SWITCH TABS</h1>
+      <div className="switching-tabs">
+        <div className="tab-items">
+          {data.map((tab, index) => {
+            return (
+              <span
+                key={index}
+                className={`tab-item ${select === index ? "active" : ""}`}
+                onClick={() => activeTab(tab, index)}
+              >
+                {tab}
+              </span>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 };
