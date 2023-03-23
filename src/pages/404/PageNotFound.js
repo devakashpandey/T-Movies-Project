297a32fabe0a3d57376b10bc5ddffbdc7c081db0
DevAkky <developerakky@gmail.com>
2023-03-23 13:13:53 +0530

    back btn added

--- src/pages/404/PageNotFound.js
+++ src/pages/404/PageNotFound.js
@@ -1,15 +1,22 @@
 import React from "react";
 import "./PageNotFound.scss";
-
 import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+import { useNavigate } from "react-router-dom";
 
 const PageNotFound = () => {
+  const navigate = useNavigate();
   return (
     <div className="pageNotFound">
       <ContentWrapper>
-        <span className="bigText">404</span>
-        <span className="smallText">Page not found!</span>
+        <span className="big-text">404</span>
+        <span className="pnf">Page not found!</span>
+        <br />
+        <button className="goHome-btn" onClick={() => navigate("/")}>
+          Go To Home
+        </button>
       </ContentWrapper>
     </div>
   );
 };
+
+export default PageNotFound;
