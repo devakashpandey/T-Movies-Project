762488256b6a4109ccb3f2358fbe0078177f13b3
DevAkky <114848776+devakashpandey@users.noreply.github.com>
2023-06-02 15:24:10 +0530

    remove genres func

--- src/redux/slices/HomeSlice.js
+++ src/redux/slices/HomeSlice.js
@@ -4,18 +4,14 @@ export const homeSlice = createSlice({
   name: "home",
   initialState: {
     url: {}, // url of movies's & poster pic
-    genres: {}, // categoris of movies like comedy, action
   },
 
   reducers: {
     getApiConfiguration: (state, action) => {
       state.url = action.payload;
     },
-    getGenre: (state, action) => {
-      state.genres = action.payload;
-    },
   },
 });
 
-export const { getApiConfiguration, getGenre } = homeSlice.actions;
+export const { getApiConfiguration } = homeSlice.actions;
 export default homeSlice.reducer;
